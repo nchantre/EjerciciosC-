2be12b28eb91e14c2c09ee0b98ec6649ba6cf376
nchantre <nelsonfabiancs@hotmail.com>
2021-11-29 18:27:21 -0500

    ajustes ejercicio 84
    29/11/2021

---
+++ Solution1/Ejercicio6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ejercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la tabla de multiplicar");
+            int dato = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(dato +"X"+i+"="+dato*i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
